efff151fc3b86a51bf668bf4c4b952d52042a4cd
Pavel Tikhonov <ptikhonov@mobiledimension.ru>
2018-02-05 18:47:18 +0300

    Initial Commit

---
+++ CDMigrationTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CDMigrationTest
+//
+//  Created by Pavel Tikhonov on 05.02.18.
+//  Copyright © 2018 Pavel Tikhonov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
